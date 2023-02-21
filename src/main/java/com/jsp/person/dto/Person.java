f29db77e6b5bd5bfd1e28bb0dfcd30742f60eb44
NikitaJadhav14 <jadhavnikita315@gmail.com>
2023-02-21 22:55:17 +0530

    First Commit

---
+++ src/main/java/com/jsp/person/dto/Person.java
@@ -0,0 +1,65 @@
+package com.jsp.person.dto;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String email;
+	private long no;
+	@ManyToMany
+	@JoinTable(name = "person_cab", joinColumns = @JoinColumn(name = "person_id"), inverseJoinColumns = @JoinColumn(name = "can_id"))
+	private List<Cab> cab;
+
+	public List<Cab> getCab() {
+		return cab;
+	}
+
+	public void setCab(List<Cab> cab) {
+		this.cab = cab;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getNo() {
+		return no;
+	}
+
+	public void setNo(long no) {
+		this.no = no;
+	}
+
+}
